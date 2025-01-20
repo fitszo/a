1fbb60930894250ea98a329454a09964899dcafa
fitszo <learneasenetwork@gmail.com>
2025-01-20 18:11:26 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: ["https://fitszo-client.vercel.app"],
     methods: ["POST", "GET"],
   })
 );
