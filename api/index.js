def71098126ccd8c95433ffd64b8363f9a229095
ankit2003kundu <136164199+ankit2003kundu@users.noreply.github.com>
2025-01-21 21:05:02 +0530

    fffff

--- api/index.js
+++ api/index.js
@@ -2,43 +2,47 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Partners_data = require("../backend/models/Partners_data");
+const Partners_data = require('../backend/models/Partners_data');
+const bodyParser = require("body-parser");
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
-const MONGO = process.env.MONGO_API_KEY;
+const PORT = process.env.PORT || 5000
+const MONGO = process.env.MONGO_API_KEY
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "https://fitszo-client.vercel.app",
-    methods: ["GET", "POST"],
-  })
-);
+app.use(cors({
+    origin: "https://fitszo-client.vercel.app/",
+    methods: ["GET", "POST"]
+}
+));
 
-mongoose.connect(MONGO, console.log("connected to mongodb"));
+mongoose.connect(MONGO,
+    console.log('connected to mongodb')
+)
 
-app.get("/", (req, res) => {
-  res.json("Hello this is the backend");
-});
+app.get('/', (req, res) => {
+    res.json("Hello this is the backend");
+})
 
 app.post("/postgymdata", async (req, res) => {
-  const data = {
-    Partner_name: req.body.username,
-    contact_no: req.body.contactno,
-  };
 
-  try {
-    const data_status = await Partners_data.create(data);
-    res.json(data_status);
-    console.log(data_status);
-    res.status(200);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json(error);
-  }
-});
+    const data = {
+        "Partner_name": req.body.username,
+        "contact_no": req.body.contactno,
+    }
+
+    try {
+        const data_status = await Partners_data.create(data);
+        res.json(data_status);
+        console.log(data_status);
+        res.status(200)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json(error)
+    }
+
+})
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
