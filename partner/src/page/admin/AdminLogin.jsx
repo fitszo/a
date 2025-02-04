8ac3e5eeab0566c96baf824bb9ac112bd74d8400
ankit2003kundu <136164199+ankit2003kundu@users.noreply.github.com>
2025-02-04 14:23:01 +0530

    hi

--- partner/src/page/admin/AdminLogin.jsx
+++ partner/src/page/admin/AdminLogin.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { setToken } from "../../utils/auth";
+import "./hello.css";
 
 export default function AdminLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = async () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
     const response = await fetch("https://server.fitszo.com/api/admin/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -19,25 +23,46 @@ export default function AdminLogin() {
       setToken(data.token, "admin");
       navigate("/admin/dashboard");
     } else {
-      alert(data.message);
+      setError(data.message);
     }
   };
 
   return (
-    <div>
-      <h2>Admin Login</h2>
-      <input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <>
+      <div className="background-black">
+        <div className="admin-container">
+          <h2 className="admin-h2">Admin Login</h2>
+          <form onSubmit={handleLogin} className="admin-form">
+            <div className="input-group-admin">
+              <label for="email" className="input-group-admin-label">
+                Email
+              </label>
+              <input
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="input-group-admin-input"
+              />
+            </div>
+            <div className="input-group-admin">
+              <label for="password" className="input-group-admin-label">
+                Password
+              </label>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input-group-admin-input"
+              />
+            </div>
+            <button type="submit" className="admin-login-button">
+              Login
+            </button>
+            {error && <p className="admin-login-error">{error}</p>}
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
