59197ac9f49f0d6e92968d0bc9ea6be128e378eb
ankit2003kundu <136164199+ankit2003kundu@users.noreply.github.com>
2025-02-05 19:58:25 +0530

    hi

--- api/models/AdminLogin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AdminSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  lastLogin: Date,
-});
-
-export default mongoose.model("Admin", AdminSchema, "admin_logins");
--- partner/src/App.jsx
+++ partner/src/App.jsx
@@ -1,66 +1,7 @@
-import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Login from "./page/Login";
-import PartnerDashboard from "./component/PartnerDashboard";
-import AdminLogin from "./page/AdminLogin";
-import AdminDashboard from "./component/AdminDashboard";
-import CreatePartner from "./component/CreatePartner";
-import ViewPartners from "./component/ViewPartners";
-
 function App() {
-  const [partnerAuth, setPartnerAuth] = useState(false);
-  const [adminAuth, setAdminAuth] = useState(false);
-
-  useEffect(() => {
-    // Check Partner Authentication
-    fetch("https://server.fitszo.com/api/partner/details", {
-      credentials: "include",
-    })
-      .then((res) => {
-        if (res.ok) setPartnerAuth(true);
-      })
-      .catch(() => {});
-
-    // Check Admin Authentication
-    fetch("https://server.fitszo.com/api/admin/details", {
-      credentials: "include",
-    })
-      .then((res) => {
-        if (res.ok) setAdminAuth(true);
-      })
-      .catch(() => {});
-  }, []);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={partnerAuth ? <Navigate to="/dashboard" /> : <Login />}
-      />
-      <Route
-        path="/dashboard"
-        element={partnerAuth ? <PartnerDashboard /> : <Navigate to="/" />}
-      />
-      <Route
-        path="/admin"
-        element={
-          adminAuth ? <Navigate to="/admin/dashboard" /> : <AdminLogin />
-        }
-      />
-      <Route
-        path="/admin/dashboard"
-        element={adminAuth ? <AdminDashboard /> : <Navigate to="/admin" />}
-      />
-      <Route
-        path="/admin/partners/create"
-        element={adminAuth ? <CreatePartner /> : <Navigate to="/admin" />}
-      />
-      <Route
-        path="/admin/partners/view"
-        element={adminAuth ? <ViewPartners /> : <Navigate to="/admin" />}
-      />
-    </Routes>
+    <div><h1>hi</h1></div>
   );
 }
 
